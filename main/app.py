49e4412a7c3c252e344488386e61ae04cab4b325
ritchie254 <richardoduor012@gmail.com>
2024-09-16 20:37:39 +0300

    working on the database functionality

--- main/app.py
+++ main/app.py
@@ -23,10 +23,10 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship('Post', backref='author', lazy='joined')
 
     def __repr__(self):
-        return f"User ('{self.username}', '{self.email}'"
+        return f"User ('{self.username}', '{self.email}')"
 
 
 class Post(db.Model):
