5150db619beb7fddcb2b5d55894ae9a54aada959
ritchie254 <richardoduor012@gmail.com>
2024-09-17 20:36:51 +0300

    working on the database functionality

--- main/static/res.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#memu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('open');
-}
